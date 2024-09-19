aea75434644b6674022abc0799af46a064404b3f
Eric Turcotte <eric.turcotte4@usherbrooke.ca>
2024-09-18 20:03:24 -0400

    Ajustements des classes Piece et Project

--- src/main/java/com/capcalculator/model/Piece.java
+++ src/main/java/com/capcalculator/model/Piece.java
@@ -1,11 +1,180 @@
 package com.capcalculator.model;
 
+import org.json.JSONObject;
+
 public class Piece {
-    private String name;
+    private String title;
     private String material;
     private int height;
     private int width;
+    private double materialCost;
     private int cutTime;
-    private boolean sanding;
+    private double cutCost;
+    private boolean baseSandingCostOnCutTime;
     private double sandingCost;
+    private boolean basePaintCostOnMaterialArea;
+    private double paintingCost;
+    private double adjustment;
+    private double cleaningAndMaintenanceCost;
+    private double subTotal;
+    private double total;
+
+    public Piece(String title, String material, int height, int width, double materialCost, int cutTime, double cutCost, boolean baseSandingCostOnCutTime, double sandingCost, boolean basePaintCostOnMaterialArea, double paintingCost, double adjustment, double cleaningAndMaintenanceCost, double subTotal, double total) {
+        this.title = title;
+        this.material = material;
+        this.height = height;
+        this.width = width;
+        this.materialCost = materialCost;
+        this.cutTime = cutTime;
+        this.cutCost = cutCost;
+        this.baseSandingCostOnCutTime = baseSandingCostOnCutTime;
+        this.sandingCost = sandingCost;
+        this.basePaintCostOnMaterialArea = basePaintCostOnMaterialArea;
+        this.paintingCost = paintingCost;
+        this.adjustment = adjustment;
+        this.cleaningAndMaintenanceCost = cleaningAndMaintenanceCost;
+        this.subTotal = subTotal;
+        this.total = total;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public double getMaterialCost() {
+        return materialCost;
+    }
+
+    public void setMaterialCost(double materialCost) {
+        this.materialCost = materialCost;
+    }
+
+    public int getCutTime() {
+        return cutTime;
+    }
+
+    public void setCutTime(int cutTime) {
+        this.cutTime = cutTime;
+    }
+
+    public double getCutCost() {
+        return cutCost;
+    }
+
+    public void setCutCost(double cutCost) {
+        this.cutCost = cutCost;
+    }
+
+    public boolean isBaseSandingCostOnCutTime() {
+        return baseSandingCostOnCutTime;
+    }
+
+    public void setBaseSandingCostOnCutTime(boolean baseSandingCostOnCutTime) {
+        this.baseSandingCostOnCutTime = baseSandingCostOnCutTime;
+    }
+
+    public double getSandingCost() {
+        return sandingCost;
+    }
+
+    public void setSandingCost(double sandingCost) {
+        this.sandingCost = sandingCost;
+    }
+
+    public boolean isBasePaintCostOnMaterialArea() {
+        return basePaintCostOnMaterialArea;
+    }
+
+    public void setBasePaintCostOnMaterialArea(boolean basePaintCostOnMaterialArea) {
+        this.basePaintCostOnMaterialArea = basePaintCostOnMaterialArea;
+    }
+
+    public double getPaintingCost() {
+        return paintingCost;
+    }
+
+    public void setPaintingCost(double paintingCost) {
+        this.paintingCost = paintingCost;
+    }
+
+    public double getAdjustment() {
+        return adjustment;
+    }
+
+    public void setAdjustment(double adjustment) {
+        this.adjustment = adjustment;
+    }
+
+    public double getCleaningAndMaintenanceCost() {
+        return cleaningAndMaintenanceCost;
+    }
+
+    public void setCleaningAndMaintenanceCost(double cleaningAndMaintenanceCost) {
+        this.cleaningAndMaintenanceCost = cleaningAndMaintenanceCost;
+    }
+
+    public double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(double subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject jo = new JSONObject();
+        jo.put("title", getTitle());
+        jo.put("material", getMaterial());
+        jo.put("height", getHeight());
+        jo.put("width", getWidth());
+        jo.put("materialCost", getMaterialCost());
+        jo.put("cutTime", getCutTime());
+        jo.put("cutCost", getCutCost());
+        jo.put("isBaseSandingCostOnCutTime", isBaseSandingCostOnCutTime());
+        jo.put("sandingCost", getSandingCost());
+        jo.put("isBasePaintCostOnMaterialArea", isBasePaintCostOnMaterialArea());
+        jo.put("paintingCost", getPaintingCost());
+        jo.put("adjustment", getAdjustment());
+        jo.put("cleaningAndMaintenanceCost", getCleaningAndMaintenanceCost());
+        jo.put("subtotal", getSubTotal());
+        jo.put("total", getTotal());
+
+        return jo;
+    }
 }
--- src/main/java/com/capcalculator/model/Project.java
+++ src/main/java/com/capcalculator/model/Project.java
@@ -1,35 +1,49 @@
 package com.capcalculator.model;
 
 import java.util.Date;
+import org.json.JSONObject;
 
 public class Project {
-    private String name;
-    private String clientName;
-    private Date endDate;
+    private String title;
+    private String client;
+    private Date deadline;
     private Piece[] pieces;
+    private float cncPrice;
+    private float sandingPrice;
+    private float paintPrice;
 
-    public String getName() {
-        return name;
+    public Project(String title, String client, Date deadline, Piece[] pieces, float cncPrice, float sandingPrice, float paintPrice) {
+        this.title = title;
+        this.client = client;
+        this.deadline = deadline;
+        this.pieces = pieces;
+        this.cncPrice = cncPrice;
+        this.sandingPrice = sandingPrice;
+        this.paintPrice = paintPrice;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getTitle() {
+        return title;
     }
 
-    public String getClientName() {
-        return clientName;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
+    public String getClient() {
+        return client;
     }
 
-    public Date getEndDate() {
-        return endDate;
+    public void setClient(String client) {
+        this.client = client;
     }
 
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
     }
 
     public Piece[] getPieces() {
@@ -38,5 +52,38 @@ public class Project {
 
     public void setPieces(Piece[] pieces) {
         this.pieces = pieces;
+    }
+
+    public float getCncPrice() {
+        return cncPrice;
+    }
+
+    public void setCncPrice(float cncPrice) {
+        this.cncPrice = cncPrice;
+    }
+
+    public float getSandingPrice() {
+        return sandingPrice;
+    }
+
+    public void setSandingPrice(float sandingPrice) {
+        this.sandingPrice = sandingPrice;
+    }
+
+    public float getPaintPrice() {
+        return paintPrice;
+    }
+
+    public void setPaintPrice(float paintPrice) {
+        this.paintPrice = paintPrice;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject jo = new JSONObject();
+        jo.put("title", getTitle());
+        jo.put("clientName", getClient());
+        jo.put("endDate", getDeadline().toString());
+
+        return jo;
     }
 }
